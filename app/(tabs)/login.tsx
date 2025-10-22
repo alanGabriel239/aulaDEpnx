6df02e5ef2b03cd4ad9c7a4481836f7259f945f7
alanGabriel239 <alan27gabriel@gmail.com>
2025-10-21 21:20:43 -0400

    criando tela de login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,15 @@
+import {View, Stylesheet, Text} from 'react-native';
+
+export default function Login(){
+    return(
+    <View>
+        <Text>
+            só Alegria!
+        </Text>
+    </View>
+    )
+}
+
+const style = Stylesheet.create({
+
+});
